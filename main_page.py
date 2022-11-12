99562b066da0d4d8865fa5cf354db05b04126185
vibhorgupta31 <vibhorgupta7@gmail.com>
2022-11-11 22:45:03 -0500

    Add folium map code

--- main_page.py
+++ main_page.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from streamlit_folium import st_folium
+import folium
 
 #Setting layout of the page
 st.set_page_config(layout = "wide") 
@@ -23,6 +25,26 @@ st.markdown("**About the project**")
 st.markdown("**The raw data**")
 st.markdown("**Processed data**")
 
+# Map / Chloropeth Creation
+
+# Metadata download
+url = ("https://raw.githubusercontent.com/python-visualization/folium/master/examples/data")
+state_geo = f"{url}/us-states.json"
+
+#Map Creation
+m = folium.Map(location=[48, -102],height='7%', zoom_start=3, min_zoom = 3, tiles="cartodb positron",)
+folium.Choropleth(
+    geo_data=state_geo,
+    data=grouped_data,
+    columns=["state", column],
+    key_on="feature.id",
+    fill_color="RdBu",
+    fill_opacity=0.7,
+    line_opacity=0.5,
+    legend_name=f"{column} (%)",
+    reset=True,
+).add_to(m)
+st_data = st_folium(m)
 
 #Footer
 st.markdown("*Fin*")
